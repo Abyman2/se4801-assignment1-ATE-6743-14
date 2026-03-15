d614f88ceb1d93e0aa7b79141915aa1034f6638b
messay <messay8>
2026-03-15 15:23:32 +0300

    Implement generic findMax method with tests

---
+++ src/test/java/com/se4801/assignment1/Assignment1Application.java
@@ -0,0 +1,15 @@
+// Abel Seleshe: ATE/6743/14
+
+package com.se4801.assignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assignment1Application.class, args);
+    }
+
+}
